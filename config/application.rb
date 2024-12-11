793a89b49a5b604bde3182a2ec4bf8c63cc69006
Matt Almeida <matt7@hey.com>
2024-12-11 11:18:16 -0500

    Setup Active Record Encryption

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ module Janus
   class Application < Rails::Application
     config.load_defaults 8.1
 
+    config.active_record.encryption.encrypt_fixtures = true
+
     config.autoload_lib(ignore: %w[assets tasks])
   end
 end
