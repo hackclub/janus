8c27f94f3576bd79ac4552962287c7381467713f
Matt Almeida <matt7@hey.com>
2024-12-16 09:57:01 -0500

    Use Steve's timezone

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ module Janus
   class Application < Rails::Application
     config.load_defaults 8.1
 
+    config.time_zone = "America/New_York"
+
     config.active_record.encryption.encrypt_fixtures = true
 
     config.autoload_lib(ignore: %w[assets tasks])
